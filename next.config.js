068d854612b59c58483f6736180022eea07f7b28
GjcjioopOrg <157663620+gjcjiooplink@users.noreply.github.com>
2025-02-23 20:22:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,6 +25,12 @@ const nextConfig = {
       'logos-world.net',
     ],
   },
+ eslint: {
+     ignoreDuringBuilds: true, // 忽略 eslint 检查
+  },
+ typescript: {
+     ignoreBuildErrors: true, // 忽略 TypeScript 检查
+  }
 }
 
 module.exports = nextConfig
